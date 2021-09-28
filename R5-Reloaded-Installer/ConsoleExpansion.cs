98f2fc6b2cc7309bb18f8975d6b87e96851430c1
Limitex <76650151+Limitex@users.noreply.github.com>
2021-09-28 20:25:13 +0900

    Add function

--- R5-Reloaded-Installer/ConsoleExpansion.cs
+++ R5-Reloaded-Installer/ConsoleExpansion.cs
@@ -22,10 +22,45 @@ namespace R5_Reloaded_Installer
             Console.Write(value);
         }
 
+        public static void LogDebug(string value)
+        {
+            LogInfo("Debug", ConsoleColor.Yellow);
+            Console.WriteLine(value);
+        }
+
         public static void LogWriteError(string value)
         {
             LogInfo("Error", ConsoleColor.Red);
             Console.Write(value);
+        }
+
+        public static bool ConsentInput(string CanselMassage = null)
+        {
+            var Trials = 5;
+            var TrialsCount = 0;
+            while (TrialsCount < Trials)
+            {
+                LogWrite("Yes No (y/n) : ");
+                var key = Console.ReadKey().Key;
+                Console.WriteLine();
+                switch (key)
+                {
+                    case ConsoleKey.Y:
+                        return true;
+                    case ConsoleKey.N:
+                        if (CanselMassage == null)
+                            LogWriteLine("The operation was canceled by the user.");
+                        else
+                            LogWriteLine(CanselMassage);
+                        return false;
+                    default:
+                        LogWriteLineError("Enter either Y or N. Type it again.");
+                        TrialsCount++;
+                        break;
+                }
+            }
+            LogWriteError("The trial limit has been reached.");
+            return false;
         }
 
         private static void LogInfo(string str, ConsoleColor color)
