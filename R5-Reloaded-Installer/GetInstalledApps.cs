8d4da2397508a92d313c09504f52fe290c37d9c9
Limitex <76650151+Limitex@users.noreply.github.com>
2021-10-02 22:55:27 +0900

    Add a class to get a list of apps

---
+++ R5-Reloaded-Installer/GetInstalledApps.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace R5_Reloaded_Installer
+{
+    class GetInstalledApps
+    {
+        private static string RegistryPath_64 = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+        private static string RegistryPath_32 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+
+        public static string[] AllList()
+        {
+            var List64 = GetUninstallList(RegistryPath_64);
+            var List32 = GetUninstallList(RegistryPath_32);
+            var ListAll = List64.Concat(List32).ToArray();
+            Array.Sort(ListAll, StringComparer.OrdinalIgnoreCase);
+            return ListAll;
+        }
+
+        private static List<string> GetUninstallList(string path)
+        {
+            var nameList = new List<string>();
+            foreach (var subKey in Registry.LocalMachine.OpenSubKey(path, false).GetSubKeyNames())
+            {
+                var subKeys = Registry.LocalMachine.OpenSubKey(path + @"\" + subKey, false);
+                var displayName = subKeys.GetValue("DisplayName");
+                if (displayName != null) nameList.Add(displayName.ToString());
+                else nameList.Add(subKey);
+            }
+            return nameList;
+        }
+    }
+}
