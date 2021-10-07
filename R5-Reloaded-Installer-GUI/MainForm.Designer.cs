9107ea9d0b55c39358926976edc175e0738bfa16
Limitex <76650151+Limitex@users.noreply.github.com>
2021-10-07 23:37:58 +0900

    Add Tab Control

--- R5-Reloaded-Installer-GUI/MainForm.Designer.cs
+++ R5-Reloaded-Installer-GUI/MainForm.Designer.cs
@@ -33,6 +33,14 @@ namespace R5_Reloaded_Installer_GUI
             this.InstallButton = new System.Windows.Forms.Button();
             this.NextButton = new System.Windows.Forms.Button();
             this.BackButton = new System.Windows.Forms.Button();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.IntroductionTabPage = new System.Windows.Forms.TabPage();
+            this.InformationTabPage = new System.Windows.Forms.TabPage();
+            this.PlaceOfInstallationTabPage = new System.Windows.Forms.TabPage();
+            this.OptionTabPage = new System.Windows.Forms.TabPage();
+            this.ProcessTabPage = new System.Windows.Forms.TabPage();
+            this.CompletionTabPage = new System.Windows.Forms.TabPage();
+            this.MainTabControl.SuspendLayout();
             this.SuspendLayout();
             // 
             // CancelButton
@@ -71,11 +79,82 @@ namespace R5_Reloaded_Installer_GUI
             this.BackButton.Text = "< Back";
             this.BackButton.UseVisualStyleBackColor = true;
             // 
+            // MainTabControl
+            // 
+            this.MainTabControl.Controls.Add(this.IntroductionTabPage);
+            this.MainTabControl.Controls.Add(this.InformationTabPage);
+            this.MainTabControl.Controls.Add(this.PlaceOfInstallationTabPage);
+            this.MainTabControl.Controls.Add(this.OptionTabPage);
+            this.MainTabControl.Controls.Add(this.ProcessTabPage);
+            this.MainTabControl.Controls.Add(this.CompletionTabPage);
+            this.MainTabControl.Location = new System.Drawing.Point(12, 12);
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(460, 308);
+            this.MainTabControl.TabIndex = 1;
+            // 
+            // IntroductionTabPage
+            // 
+            this.IntroductionTabPage.Location = new System.Drawing.Point(4, 24);
+            this.IntroductionTabPage.Name = "IntroductionTabPage";
+            this.IntroductionTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.IntroductionTabPage.Size = new System.Drawing.Size(452, 280);
+            this.IntroductionTabPage.TabIndex = 0;
+            this.IntroductionTabPage.Text = "Introduction";
+            this.IntroductionTabPage.UseVisualStyleBackColor = true;
+            // 
+            // InformationTabPage
+            // 
+            this.InformationTabPage.Location = new System.Drawing.Point(4, 24);
+            this.InformationTabPage.Name = "InformationTabPage";
+            this.InformationTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.InformationTabPage.Size = new System.Drawing.Size(452, 280);
+            this.InformationTabPage.TabIndex = 1;
+            this.InformationTabPage.Text = "Information";
+            this.InformationTabPage.UseVisualStyleBackColor = true;
+            // 
+            // PlaceOfInstallationTabPage
+            // 
+            this.PlaceOfInstallationTabPage.Location = new System.Drawing.Point(4, 24);
+            this.PlaceOfInstallationTabPage.Name = "PlaceOfInstallationTabPage";
+            this.PlaceOfInstallationTabPage.Size = new System.Drawing.Size(452, 280);
+            this.PlaceOfInstallationTabPage.TabIndex = 2;
+            this.PlaceOfInstallationTabPage.Text = "Place of installation";
+            this.PlaceOfInstallationTabPage.UseVisualStyleBackColor = true;
+            // 
+            // OptionTabPage
+            // 
+            this.OptionTabPage.Location = new System.Drawing.Point(4, 24);
+            this.OptionTabPage.Name = "OptionTabPage";
+            this.OptionTabPage.Size = new System.Drawing.Size(452, 280);
+            this.OptionTabPage.TabIndex = 3;
+            this.OptionTabPage.Text = "Option";
+            this.OptionTabPage.UseVisualStyleBackColor = true;
+            // 
+            // ProcessTabPage
+            // 
+            this.ProcessTabPage.Location = new System.Drawing.Point(4, 24);
+            this.ProcessTabPage.Name = "ProcessTabPage";
+            this.ProcessTabPage.Size = new System.Drawing.Size(452, 280);
+            this.ProcessTabPage.TabIndex = 4;
+            this.ProcessTabPage.Text = "Process";
+            this.ProcessTabPage.UseVisualStyleBackColor = true;
+            // 
+            // CompletionTabPage
+            // 
+            this.CompletionTabPage.Location = new System.Drawing.Point(4, 24);
+            this.CompletionTabPage.Name = "CompletionTabPage";
+            this.CompletionTabPage.Size = new System.Drawing.Size(452, 280);
+            this.CompletionTabPage.TabIndex = 5;
+            this.CompletionTabPage.Text = "Completion";
+            this.CompletionTabPage.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.MainTabControl);
             this.Controls.Add(this.BackButton);
             this.Controls.Add(this.NextButton);
             this.Controls.Add(this.InstallButton);
@@ -86,6 +165,7 @@ namespace R5_Reloaded_Installer_GUI
             this.MinimumSize = new System.Drawing.Size(500, 400);
             this.Name = "MainForm";
             this.Text = "R5-Reloaded Installer";
+            this.MainTabControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,6 +176,13 @@ namespace R5_Reloaded_Installer_GUI
         private System.Windows.Forms.Button InstallButton;
         private System.Windows.Forms.Button NextButton;
         private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.TabControl MainTabControl;
+        private System.Windows.Forms.TabPage IntroductionTabPage;
+        private System.Windows.Forms.TabPage InformationTabPage;
+        private System.Windows.Forms.TabPage PlaceOfInstallationTabPage;
+        private System.Windows.Forms.TabPage OptionTabPage;
+        private System.Windows.Forms.TabPage ProcessTabPage;
+        private System.Windows.Forms.TabPage CompletionTabPage;
     }
 }
 
