1402f6290e6f73f707e7aff7aa544948c484c7da
Limitex <76650151+Limitex@users.noreply.github.com>
2021-10-14 16:06:16 +0900

    Move Time class

--- R5-Reloaded-Installer-Library/Get/Download.cs
+++ R5-Reloaded-Installer-Library/Get/Download.cs
@@ -141,17 +141,4 @@ namespace R5_Reloaded_Installer_Library.Get
             else return rawDirectoryPath;
         }
     }
-    public static class Time
-    {
-        private static Stopwatch stopWatch = Stopwatch.StartNew();
-        private static float timeBuffer = nowTime;
-        private static float nowTime => stopWatch.ElapsedTicks * 0.0001f * 0.001f;
-        private static float getDelta()
-        {
-            var oldTime = timeBuffer;
-            timeBuffer = nowTime;
-            return timeBuffer - oldTime;
-        }
-        public static float deltaTime => getDelta();
-    }
 }
