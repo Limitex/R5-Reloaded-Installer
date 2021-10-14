dbb3f365367eadbc2acf1c2546d6bd15167164ff
Limitex <76650151+Limitex@users.noreply.github.com>
2021-10-14 12:28:42 +0900

    Initial commit

---
+++ R5-Reloaded-Installer-CUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5_Reloaded_Installer_CUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ R5-Reloaded-Installer-GUI/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace R5_Reloaded_Installer_GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
