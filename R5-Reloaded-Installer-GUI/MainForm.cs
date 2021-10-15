92fe5de8f04d5cc4c02155f93f386316d3aa5789
Limitex <76650151+Limitex@users.noreply.github.com>
2021-10-16 05:29:46 +0900

    Code move

--- R5-Reloaded-Installer-GUI/MainForm.cs
+++ R5-Reloaded-Installer-GUI/MainForm.cs
@@ -16,15 +16,12 @@ namespace R5_Reloaded_Installer_GUI
 
         public static bool IsRunning = true;
 
-        private static Form LogForm;
-        private static RichTextBox LogFormRichTexBox;
-
         public MainForm()
         {
             InitializeComponent();
-            LogFormInitialize();
             new ButtonOperation(this, MainTask_StartInstall);
             new GetSizeAndPath(this);
+            new LogWindow(this);
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
@@ -46,33 +43,8 @@ namespace R5_Reloaded_Installer_GUI
             MessageBox.Show(e.CreateDesktopShortcut.ToString());
             MessageBox.Show(e.AddShortcutToStartMenu.ToString());
             MessageBox.Show("MainTask");
-            if (LogForm.Visible) LogFormRichTexBox.AppendText("LogText" + "\n");
+            LogWindow.WriteLine("LogWindow");
             NextButton.Enabled = true;
-        }
-
-        private void DetailedInfoLinkLabe_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            if (!LogForm.Visible)
-            {
-                LogFormInitialize();
-                LogForm.Show();
-            }
-        }
-
-        private void LogFormInitialize()
-        {
-            LogForm = new Form() { Text = "Log Window", ShowIcon = false };
-            LogFormRichTexBox = new RichTextBox()
-            {
-                Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right,
-                Name = "logRichTextBox",
-                Location = new Point(0, 0),
-                Size = new Size(LogForm.Width - 16, LogForm.Height - 39),
-                BackColor = Color.White,
-                ReadOnly = true,
-                HideSelection = false
-            };
-            LogForm.Controls.Add(LogFormRichTexBox);
         }
     }
 }
