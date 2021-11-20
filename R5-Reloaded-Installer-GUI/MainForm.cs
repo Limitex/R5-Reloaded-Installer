d6877159bb90e89cc548cee7b8a96ad44b9acfd4
Limitex <76650151+Limitex@users.noreply.github.com>
2021-11-20 12:42:44 +0900

    Create projects

---
+++ R5-Reloaded-Installer-GUI/MainForm.cs
@@ -0,0 +1,10 @@
+namespace R5_Reloaded_Installer_GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ R5-Reloaded-Installer-GUI/Program.cs
@@ -0,0 +1,15 @@
+namespace R5_Reloaded_Installer_GUI
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
