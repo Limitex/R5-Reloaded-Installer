83160d07ac8c13733fa3a6ecd1069a4906a16bc1
Limitex <76650151+Limitex@users.noreply.github.com>
2021-11-29 14:49:57 +0900

    Create Time.cs

---
+++ R5-Reloaded-Installer-Library/Other/Time.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace R5_Reloaded_Installer_Library.Other
+{
+    public static class Time
+    {
+        private static Stopwatch stopWatch = Stopwatch.StartNew();
+        private static float timeBuffer = nowTime;
+        private static float nowTime => stopWatch.ElapsedTicks * 0.0001f * 0.001f;
+        private static float getDelta()
+        {
+            var oldTime = timeBuffer;
+            timeBuffer = nowTime;
+            return timeBuffer - oldTime;
+        }
+        public static float deltaTime => getDelta();
+    }
+}
+
