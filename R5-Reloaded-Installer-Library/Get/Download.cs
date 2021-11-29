b16958c1d058702ee2167cd55e1f949ff6247149
Limitex <76650151+Limitex@users.noreply.github.com>
2021-11-29 20:26:49 +0900

    Create Download.cs

---
+++ R5-Reloaded-Installer-Library/Get/Download.cs
@@ -0,0 +1,24 @@
+﻿using R5_Reloaded_Installer_Library.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R5_Reloaded_Installer_Library.Get
+{
+    public class Download : IDisposable
+    {
+        private static string WorkingDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "R5-Reloaded-Installer");
+        
+        public Download()
+        {
+            DirectoryExpansion.CreateOverwrite(WorkingDirectoryPath);
+        }
+
+        public void Dispose()
+        {
+            DirectoryExpansion.DirectoryDelete(WorkingDirectoryPath);
+        }
+    }
+}
