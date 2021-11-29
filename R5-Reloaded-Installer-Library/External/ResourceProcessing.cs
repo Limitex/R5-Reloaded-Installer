bf3af90bee2fbd8c0d2a2a447c1a9eb93e21ba9d
Limitex <76650151+Limitex@users.noreply.github.com>
2021-11-29 23:02:18 +0900

    Move

--- R5-Reloaded-Installer-Library/External/ResourceProcessing.cs
+++ R5-Reloaded-Installer-Library/External/ResourceProcessing.cs
@@ -5,16 +5,17 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace R5_Reloaded_Installer_Library.Resource
+namespace R5_Reloaded_Installer_Library.External
 {
     public static class ResourceProcessing
     {
-        public static void ExportingFile(string path, string resource)
+        public static string ExportingFile(string path, string resource)
         {
             var assembly = Assembly.GetExecutingAssembly();
             using var stream = assembly.GetManifestResourceStream("R5_Reloaded_Installer_Library.Resources." + resource);
             if (stream == null) throw new Exception("The assembly does not have the specified file.");
             File.WriteAllBytes(path, GetByteArrayFromStream(stream));
+            return path;
         }
 
         private static byte[] GetByteArrayFromStream(Stream stream)
