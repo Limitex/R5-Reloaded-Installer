565f0e45a8beb23c3321d76cb2407d52a5e174c6
Limitex <76650151+Limitex@users.noreply.github.com>
2021-11-29 18:07:54 +0900

    Update GitHub.cs

--- R5-Reloaded-Installer-Library/Get/GitHub.cs
+++ R5-Reloaded-Installer-Library/Get/GitHub.cs
@@ -11,13 +11,13 @@ namespace R5_Reloaded_Installer_Library.Get
 {
     public static class GitHub
     {
-        public static async Task<string[]> GetLatestRelease(string owner, string repo)
+        public static string[] GetLatestRelease(string owner, string repo)
         {
             var api = PathExpansion.Combine("https://api.github.com/", "repos", owner, repo, "releases", "latest");
             var request = new HttpRequestMessage(HttpMethod.Get, api);
             request.Headers.Add("User-Agent", "0");
-            var result = await new HttpClient().SendAsync(request).Result.Content.ReadAsStringAsync();
-            var json = JObject.Parse(result);
+            var result = new HttpClient().SendAsync(request).Result.Content.ReadAsStringAsync(); result.Wait();
+            var json = JObject.Parse(result.Result);
             var links = new List<string>();
             foreach (var link in json["assets"] ?? string.Empty)
                 links.Add((string?)link["browser_download_url"] ?? string.Empty);
