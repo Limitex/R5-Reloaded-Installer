83b56fd6942f7c281c8dd0edb30695df453cc443
Limitex <76650151+Limitex@users.noreply.github.com>
2021-12-04 20:29:21 +0900

    Update Download.cs

--- R5-Reloaded-Installer-Library/Get/Download.cs
+++ R5-Reloaded-Installer-Library/Get/Download.cs
@@ -1,6 +1,8 @@
-﻿using R5_Reloaded_Installer_Library.IO;
+﻿using R5_Reloaded_Installer_Library.External;
+using R5_Reloaded_Installer_Library.IO;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +13,38 @@ namespace R5_Reloaded_Installer_Library.Get
     {
         private static string WorkingDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "R5-Reloaded-Installer");
         
+        private ResourceProcess aria2c;
+        private ResourceProcess sevenZip;
+        private ResourceProcess transmission;
+
         public Download()
         {
             DirectoryExpansion.CreateOverwrite(WorkingDirectoryPath);
+
+            aria2c = new ResourceProcess(WorkingDirectoryPath, "aria2c");
+            sevenZip = new ResourceProcess(WorkingDirectoryPath, "seven-za");
+            transmission = new ResourceProcess(WorkingDirectoryPath, "transmission");
+
+            aria2c.ResourceProcessReceives += new ResourceProcessEventHandler(Output);
+            sevenZip.ResourceProcessReceives += new ResourceProcessEventHandler(Output);
+            transmission.ResourceProcessReceives += new ResourceProcessEventHandler(Output);
+
+            //aria2c.Run("","");
+            //sevenZip.Run("","");
+            //transmission.Run("","");
         }
 
         public void Dispose()
         {
+            aria2c.Dispose();
+            sevenZip.Dispose();
+            transmission.Dispose();
             DirectoryExpansion.DirectoryDelete(WorkingDirectoryPath);
+        }
+
+        public void Output(object sender, DataReceivedEventArgs outLine)
+        {
+            Console.WriteLine(outLine.Data);
         }
     }
 }
